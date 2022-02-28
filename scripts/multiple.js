e71072d25eb3a0e0d6bbecb5bb677e046707f873
rizkimahendra17 <riskimhndr170@gmail.com>
2022-02-28 14:43:26 +0700

    javascript module

---
+++ scripts/multiple.js
@@ -0,0 +1,11 @@
+const  company = "Calon Programmer Handal Amiin";
+
+function sum (first, second){
+    return first + second;
+}
+
+class Company {
+
+}
+
+export {company, sum, Company}
---
+++ scripts/say.js
@@ -0,0 +1,10 @@
+//ditambah export agar bisa di akses keluar, karna kita pakai module
+
+export function sayHello(name) {
+    console.info(`Hello ${name}`);
+}
+
+export function sayGoodBye(name) {
+        console.info(`Good By ${name}`);
+}
+
